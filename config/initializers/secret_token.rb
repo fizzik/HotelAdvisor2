9da1954845b2de339d66c28a2413819b4117c6a5
VitaleksPC <vitalekspc@ubuntu.(none)>
2013-04-16 18:58:35 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HotelAdvisor::Application.config.secret_token = '9196cc95f2e5d31727e6b4ad7aa87284b938760a505c62cdffb9cad2edfbd3220c9ca922e19148677737f37574ccb7d046adf1f47c2c0f9755c40d5708e030f8'
